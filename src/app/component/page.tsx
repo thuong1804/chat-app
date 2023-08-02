1309b7c0191f985e555700f6992d6179a2ec770c
thuong1804 <thuongtvt30@gmail.com>
2023-08-02 11:02:41 +0700

    update

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -1,11 +1,21 @@
-import { AuthContextProvider } from "../context/AuthContext";
+"use client";
 import SideBar from "./sidebar";
+import styled from "styled-components";
 
+const StyledContainer = styled.div`
+  display: flex;
+`;
+
+const StyledMain = styled.h1`
+  color: gray;
+  padding: 30px;
+`;
 const PageComponent = () => {
   return (
-    <>
-      <SideBar></SideBar>;
-    </>
+    <StyledContainer>
+      <SideBar></SideBar>
+      <StyledMain>Please choose conversation</StyledMain>
+    </StyledContainer>
   );
 };
 export default PageComponent;
